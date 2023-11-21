97d1a2d75f687e93e39defac94c75b75827fa5f4
hugo-w1 <144124678+hugo-w1@users.noreply.github.com>
2023-11-21 15:35:03 +0100

    Add files via upload

---
+++ AI_Vädering_API/Controllers/AiController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AI_Vädering_APIML.Model;
+
+namespace AI_Vädering_API.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+
+		[HttpGet(Name = "valuation")]
+		public IEnumerable<PricePrediction> Get(string Kommun, float BoArea, float Rum, float TomtArea)
+		{
+
+			Console.WriteLine("Kommun: " + Kommun);
+			Console.WriteLine("BoArea: " + BoArea.ToString());
+			Console.WriteLine("Rum: " + Rum.ToString());
+			Console.WriteLine("TomtArea: " + TomtArea.ToString());
+			Console.WriteLine("----------------------");
+
+
+			//Ai Price Predictor
+			ModelInput sampleData = new ModelInput()
+			{
+				Kommun = Kommun,
+				BoArea = BoArea.ToString(),
+				Rum = Rum,
+				TomtArea = TomtArea,
+			};
+
+			//Load model and predict output
+			var result = ConsumeModel.Predict(sampleData);
+
+
+			return Enumerable.Range(1, 1).Select(index => new PricePrediction
+			{
+				Pris = result.Score
+			})
+			.ToArray();
+		}
+	}
+}
---
+++ AI_Vädering_API/PricePrediction.cs
@@ -0,0 +1,7 @@
+namespace AI_Vädering_API
+{
+    public class PricePrediction
+    {
+        public float Pris { get; set; }
+    }
+}
